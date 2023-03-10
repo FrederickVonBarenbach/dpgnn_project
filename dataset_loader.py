Fixed major bug in dataloader in train/test masking. Also added lots of plotting stuff.

dataset_loader.py
@@ -8,11 +8,22 @@ def load_dataset(name):
         from torch_geometric.datasets import OGB_MAG
         dataset = OGB_MAG(root='./datasets/ogb_mag', preprocess='metapath2vec')[0]
         dataset = Data(x=dataset.x_dict['paper'],
-                    edge_index=dataset.edge_index_dict[('paper', 'cites', 'paper')],
-                    y=dataset.y_dict['paper'])
-        # add some transformations
-        dataset = add_self_edges(dataset)
-        dataset = make_undirected(dataset)
+                       edge_index=dataset.edge_index_dict[('paper', 'cites', 'paper')],
+                       y=dataset.y_dict['paper'],
+                       train_mask = dataset.train_mask_dict['paper'],
+                       test_mask = dataset.test_mask_dict['paper'])
+        # # add some transformations
+        # dataset = add_self_edges(dataset)
+        # dataset = make_undirected(dataset)
+        # get number of classes
+        num_classes = torch.unique(dataset['y']).size(dim=0)
+    elif name == "reddit":
+        # load dataset to datasets
+        from torch_geometric.datasets import Reddit
+        dataset = Reddit(root='./datasets/reddit')[0]
+        # # add some transformations
+        # dataset = add_self_edges(dataset)
+        # dataset = make_undirected(dataset)
         # get number of classes
         num_classes = torch.unique(dataset['y']).size(dim=0)
     return dataset, num_classes

main.py
@@ -3,7 +3,7 @@ import torch
 import pandas as pd
 import gc
 from csv import writer
-from configs.config import config, experiments, iterations
+from configs.config import config, experiments, iterations, logging
 from torch_geometric.loader import NeighborLoader
 from model import *
 from train_test import *
@@ -11,6 +11,8 @@ from analysis import *
 from loader import *
 from dataset_loader import *
 from pyvacy import optim, analysis
+
+MAX_DEGREE = 100 # this is just so that graphs can fit in GPU
 
 # TODO: Make it specify the output file
 def main():
@@ -42,11 +44,11 @@ def main():
       # run experiment
       # TODO: make it so that it dynamically returns the necessary metric
       if exp_config["setup"] == "original":
-        run_original_experiment(exp_config, data_file, wordy=False)
+        run_original_experiment(exp_config, data_file, wordy=logging)
       elif exp_config["setup"] == "ours":
-        run_our_experiment(exp_config, data_file, wordy=False)
+        run_our_experiment(exp_config, data_file, wordy=logging)
       elif exp_config["setup"] == "non-dp":
-        run_non_private_experiment(exp_config, data_file, wordy=False)
+        run_non_private_experiment(exp_config, data_file, wordy=logging)
 
 
 def run_non_private_experiment(config, data_file, wordy=False):
@@ -65,23 +67,19 @@ def run_non_private_experiment(config, data_file, wordy=False):
 
   # prepare data
   dataset, num_classes = load_dataset(config["dataset"])
-  if hasattr(dataset, 'train_mask') and hasattr(dataset, 'test_mask'):
-    train_dataset, test_dataset = apply_train_test_mask(dataset)
-  else:
-    train_dataset, test_dataset = train_test_split(dataset, 0.2)
-
-  # data parameters
-  n, d = train_dataset.x.size()
-  n_test = test_dataset.x.size(dim=0)
+  if not hasattr(dataset, 'train_mask'):
+    train_test_split(dataset, 0.2)
 
   # setup loaders
-  train_loader = NeighborLoader(train_dataset, 
-                                num_neighbors=[-1] * config["r_hop"],
+  train_loader = NeighborLoader(dataset, 
+                                num_neighbors=[MAX_DEGREE] * config["r_hop"],
                                 batch_size=config["batch_size"],
-                                shuffle=True)
-  test_loader = NeighborLoader(test_dataset,
-                               num_neighbors=[-1] * config["r_hop"],
-                               batch_size=config["batch_size"])
+                                shuffle=True,
+                                input_nodes=dataset.train_mask)
+  test_loader = NeighborLoader(dataset,
+                               num_neighbors=[MAX_DEGREE] * config["r_hop"],
+                               batch_size=config["batch_size"],
+                               input_nodes=dataset.test_mask)
 
   # setup model
   model = GNN(config["encoder_dimensions"], config["decoder_dimensions"], config["r_hop"], config["dropout"]).to(config["device"])
@@ -93,7 +91,7 @@ def run_non_private_experiment(config, data_file, wordy=False):
 
   # train/test
   t = 1
-  max_iters = 10000
+  max_iters = 4000
   while True:
     # train if haven't expended all of budget
     if t < max_iters:
@@ -133,35 +131,36 @@ def run_original_experiment(config, data_file, wordy=False):
 
   # prepare data
   dataset, num_classes = load_dataset(config["dataset"])
-  if hasattr(dataset, 'train_mask') and hasattr(dataset, 'test_mask'):
-    train_dataset, test_dataset = apply_train_test_mask(dataset)
-  else:
-    train_dataset, test_dataset = train_test_split(dataset, 0.2)
+  if not hasattr(dataset, 'train_mask'):
+    train_test_split(dataset, 0.2)
 
   # get clipping threshold by using clipping_percentile of the gradients
-  clipping_threshold = get_clipping_threshold(train_dataset, config)
+  clipping_threshold = get_clipping_threshold(dataset, config) # TODO: use train_mask
   row["clipping_threshold"] = clipping_threshold
 
   # get sigma according to the equation in section 6
   sigma = get_sigma(config, clipping_threshold)
   row["sigma"] = sigma
 
-  # data parameters
-  n, d = train_dataset.x.size()
-  n_test = test_dataset.x.size(dim=0)
-
   # setup loaders
-  sampled_dataset = sample_edgelists(train_dataset, config["degree_bound"])
+  sampled_dataset = sample_edgelists(dataset, config["degree_bound"])
   train_loader = NeighborLoader(sampled_dataset, 
                                 num_neighbors=[-1] * config["r_hop"],
                                 batch_size=config["batch_size"],
+                                input_nodes=sampled_dataset.train_mask,
                                 shuffle=True)
-  test_loader = NeighborLoader(test_dataset,
-                               num_neighbors=[-1] * config["r_hop"],
-                               batch_size=config["batch_size"])
+  test_loader = NeighborLoader(dataset,
+                               num_neighbors=[MAX_DEGREE] * config["r_hop"],
+                               batch_size=config["batch_size"],
+                               input_nodes=dataset.test_mask)
+  non_priv_train_loader = NeighborLoader(dataset,
+                                         num_neighbors=[MAX_DEGREE] * config["r_hop"],
+                                         batch_size=config["batch_size"],
+                                         input_nodes=dataset.train_mask,
+                                         shuffle=True)
 
   # search for alpha
-  alpha, gamma = search_for_alpha(n, sigma, clipping_threshold, config)
+  alpha, gamma = search_for_alpha(dataset.train_mask.sum().item(), sigma, clipping_threshold, config)
   row["alpha"] = alpha
   row["gamma"] = gamma
 
@@ -182,7 +181,7 @@ def run_original_experiment(config, data_file, wordy=False):
       if wordy:
         print(f"Memory reserved: {torch.cuda.memory_reserved(0)/1024**3:>0.2f} GB, memory allocated: {torch.cuda.memory_allocated(0)/1024**3:>0.2f} GB")
         print("Training step:", t)
-      _, train_acc = batch_test(next(iter(train_loader)), "TRAIN", model, loss_fn, wordy=wordy)
+      _, train_acc = batch_test(next(iter(non_priv_train_loader)), "TRAIN", model, loss_fn, wordy=wordy)
       _, test_acc = test(test_loader, "TEST", model, loss_fn, wordy=wordy)
       if wordy:
         print(" Optimizer Achieves ({:>0.1f}, {})-DP".format(curr_epsilon, config["delta"]))
@@ -214,10 +213,8 @@ def run_our_experiment(config, data_file, wordy=False):
 
   # prepare data
   dataset, num_classes = load_dataset(config["dataset"])
-  if hasattr(dataset, 'train_mask') and hasattr(dataset, 'test_mask'):
-    train_dataset, test_dataset = apply_train_test_mask(dataset)
-  else:
-    train_dataset, test_dataset = train_test_split(dataset, 0.2)
+  if not hasattr(dataset, 'train_mask'):
+    train_test_split(dataset, 0.2)
 
   # get clipping threshold by using clipping_percentile of the gradients
   clipping_threshold = get_clipping_threshold(train_dataset, config)
@@ -227,17 +224,19 @@ def run_our_experiment(config, data_file, wordy=False):
   sigma = get_sigma(config, clipping_threshold)
   row["sigma"] = sigma
 
-  # data parameters
-  n, d = train_dataset.x.size()
-  n_test = test_dataset.x.size(dim=0)
-
   # setup loaders
-  test_loader = NeighborLoader(test_dataset,
-                               num_neighbors=[-1] * config["r_hop"],
-                               batch_size=config["batch_size"])
+  test_loader = NeighborLoader(dataset,
+                               num_neighbors=[MAX_DEGREE] * config["r_hop"],
+                               batch_size=config["batch_size"],
+                               input_nodes=dataset.test_mask)
+  non_priv_train_loader = NeighborLoader(dataset,
+                                         num_neighbors=[MAX_DEGREE] * config["r_hop"],
+                                         batch_size=config["batch_size"], 
+                                         input_nodes=dataset.train_mask,
+                                         shuffle=True)
 
   # search for alpha
-  alpha, gamma = search_for_alpha(n, sigma, clipping_threshold, config)
+  alpha, gamma = search_for_alpha(dataset.train_mask.sum().item(), sigma, clipping_threshold, config)
   row["alpha"] = alpha
   row["gamma"] = gamma
 
@@ -249,10 +248,11 @@ def run_our_experiment(config, data_file, wordy=False):
   # train/test
   t = 1
   while True:
-    sampled_dataset = sample_edgelists(train_dataset, config["degree_bound"])
+    sampled_dataset = sample_edgelists(dataset, config["degree_bound"])
     train_loader = NeighborLoader(sampled_dataset, 
                                   num_neighbors=[-1] * config["r_hop"],
                                   batch_size=config["batch_size"],
+                                  input_nodes=sampled_dataset.train_mask,
                                   shuffle=True)
     curr_epsilon = get_epsilon(gamma, t, alpha, config["delta"])
     # train if haven't expended all of budget
@@ -263,7 +263,7 @@ def run_our_experiment(config, data_file, wordy=False):
       if wordy:
         print(f"Memory reserved: {torch.cuda.memory_reserved(0)/1024**3:>0.2f} GB, memory allocated: {torch.cuda.memory_allocated(0)/1024**3:>0.2f} GB")
         print("Training step:", t)
-      _, train_acc = batch_test(next(iter(train_loader)), "TRAIN", model, loss_fn, wordy=wordy)
+      _, train_acc = batch_test(next(iter(non_priv_train_loader)), "TRAIN", model, loss_fn, wordy=wordy)
       _, test_acc = test(test_loader, "TEST", model, loss_fn, wordy=wordy)
       if wordy:
         print(" Optimizer Achieves ({:>0.1f}, {})-DP".format(curr_epsilon, config["delta"]))
@@ -304,6 +304,7 @@ def get_clipping_threshold(dataset, config):
   dataloader = NeighborLoader(sampled_dataset, 
                               num_neighbors=[-1] * config["r_hop"],
                               batch_size=config["batch_size"],
+                              input_nodes=sampled_dataset.train_mask,
                               shuffle=True)
   clipping_threshold = config["clipping_multiplier"] * get_gradient_percentile(model, loss_fn, dataloader, config["clipping_percentile"])
   torch.cuda.empty_cache()
@@ -324,7 +325,12 @@ def get_optimizer(config, clipping_threshold, sigma, model):
     optimizer = optim.DPAdam(l2_norm_clip=clipping_threshold, noise_multiplier=sigma, batch_size=config["batch_size"],
                              params=model.parameters(), lr=config["lr"], weight_decay=config["weight_decay"])
   elif config["optimizer"] == "DPAdamFixed":
-    pass
+    from dp_nlp.adam_corr import AdamCorr
+    # TODO: What is eps_root?
+    optimizer = AdamCorr(dp_l2_norm_clip=clipping_threshold, dp_noise_multiplier=sigma, dp_batch_size=config["batch_size"],
+                         eps_root=1e-8, params=model.parameters(), lr=config["lr"], weight_decay=config["weight_decay"])
+  elif config["optimizer"] == "Adam":
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=config["lr"], weight_decay=config["weight_decay"])
   return optimizer
 
 

plot.py
@@ -9,30 +9,90 @@ from math import ceil
 comparison_colors = ['blue', 'red', 'green', 'orange', 'purple']
 value_styles = ['solid', 'dashed', 'dotted']
 
-def plot_graph_line(df):
+def plot_heatmap(df):
     df = df.loc[df["epsilon"] <= epsilon_bound]
-    n_rows = len(list(variables["rows"].values())[0])
-    n_columns = len(list(variables["columns"].values())[0])
-    n_comparisons = len(list(comparisons.values())[0])
+    n_rows = len(list(variables["rows"].values())[0]) if "rows" in variables else 1
+    n_columns = len(list(variables["columns"].values())[0]) if "columns" in variables else 1
 
-    # get y limits
-    max_y = 0
-    for i in range(len(values)):
-        max_y = max(max_y, df[values[i]].max())
-    max_y = 1.1*max_y
-
+    # make subplots
     fig, axs = plt.subplots(n_rows, n_columns, figsize=(2 + 4*n_columns, 1 + 4*n_rows))
-    plt.suptitle(axes[1] + " vs " + axes[0] + " plot")
+    if n_columns == 1 and n_rows == 1:
+        axs = np.array([[axs]])
+    else:
+        axs = np.reshape(axs, (n_rows, n_columns))
+    plt.suptitle(values[0] + " heatmap")
     fig.tight_layout()
+    # plot each of the subplots
     for row in range(n_rows):
         for column in range(n_columns):
             plt.subplot(n_rows, n_columns, row*n_columns + column + 1)
             df_setting = df
             # filter for given row and column
-            for (key, value) in variables["rows"].items():
-                df_setting = df_setting.loc[df_setting[key] == value[row]]
-            for (key, value) in variables["columns"].items():
-                df_setting = df_setting.loc[df_setting[key] == value[column]]
+            if "rows" in variables:
+                for (key, value) in variables["rows"].items():
+                    df_setting = df_setting.loc[df_setting[key] == value[row]]
+            if "columns" in variables:
+                for (key, value) in variables["columns"].items():
+                    df_setting = df_setting.loc[df_setting[key] == value[column]]
+            # make plot
+            df_setting = df_setting.groupby([axes[0], axes[1], "step", *(variables["best"])], as_index=False).mean(numeric_only=True)
+            df_setting = df_setting.loc[df_setting.groupby([axes[0], axes[1]])["epsilon"].idxmax()]
+            with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+                print(df_setting)
+            df_setting = pd.pivot_table(df_setting, values=values[0], index=[axes[0]], columns=[axes[1]], aggfunc=np.max)
+            sns.heatmap(df_setting, annot=True, fmt=".2%", vmax=value_range[1], vmin=value_range[0])
+            # subplot titles
+            if row == 0 and "columns" in variables:
+                title = ""
+                line = 0
+                for (key, value) in variables["columns"].items():
+                    entry = key + ": " + str(value[column]) + ", "
+                    line += len(entry)
+                    if (line > 26): # if line is longer than 26 chars, add new line
+                        title += "\n"
+                        line = 0
+                    title += entry
+                title = title[:-2]
+                plt.text(0.5, 1.05, title, fontsize=12, 
+                         horizontalalignment='center', verticalalignment='center', transform=axs[row, column].transAxes)
+            if column == 0 and "rows" in variables:
+                plt.ylabel(axes[1])
+                title = ""
+                for (key, value) in variables["rows"].items():
+                    title += key + ": " + str(value[row]) + ", "
+                title = title[:-2]
+                plt.text(-0.25, 0.5, title, fontsize=12, rotation=90,
+                         horizontalalignment='center', verticalalignment='center', transform=axs[row, column].transAxes)
+    fig.tight_layout()
+    plt.savefig(storepath)
+
+
+def plot_graph_line(df):
+    df = df.loc[df["epsilon"] <= epsilon_bound]
+    n_rows = len(list(variables["rows"].values())[0]) if "rows" in variables else 1
+    n_columns = len(list(variables["columns"].values())[0]) if "columns" in variables else 1
+    n_comparisons = len(list(comparisons.values())[0])
+
+    # make subplots
+    fig, axs = plt.subplots(n_rows, n_columns, figsize=(2 + 4*n_columns, 1 + 4*n_rows))
+    if n_columns == 1 and n_rows == 1:
+        axs = np.array([[axs]])
+    else:
+        axs = np.reshape(axs, (n_rows, n_columns))
+    plt.suptitle(axes[1] + " vs " + axes[0] + " plot")
+    fig.tight_layout()
+    # plot each of the subplots
+    for row in range(n_rows):
+        for column in range(n_columns):
+            plt.subplot(n_rows, n_columns, row*n_columns + column + 1)
+            df_setting = df
+            # filter for given row and column
+            if "rows" in variables:
+                for (key, value) in variables["rows"].items():
+                    df_setting = df_setting.loc[df_setting[key] == value[row]]
+            if "columns" in variables:
+                for (key, value) in variables["columns"].items():
+                    df_setting = df_setting.loc[df_setting[key] == value[column]]
             # filter comparisons
             for comparison in range(n_comparisons):
                 df_comparison = df_setting
@@ -40,7 +100,6 @@ def plot_graph_line(df):
                 for (key, value) in comparisons.items():
                     df_comparison = df_comparison.loc[df_comparison[key] == value[comparison]]
                     label_prefix += value[comparison] + " "
-
                 # get average according to the axes
                 if axes[0] != "step":
                     df_comparison = df_comparison.groupby([axes[0], "step"], as_index=False).mean(numeric_only=True)
@@ -53,16 +112,22 @@ def plot_graph_line(df):
                     plt.plot(x, y, label=label_prefix + values[value], color=comparison_colors[comparison], linestyle=value_styles[value])
                     plt.xlabel(axes[0])
                 plt.legend()
-                plt.ylim((0, max_y))
+                plt.ylim((value_range[0], value_range[1]))
             # subplot titles
-            if row == 0:
+            if row == 0 and "columns" in variables:
                 title = ""
+                line = 0
                 for (key, value) in variables["columns"].items():
-                    title += key + ": " + str(value[column]) + ", "
+                    entry = key + ": " + str(value[column]) + ", "
+                    line += len(entry)
+                    if (line > 26): # if line is longer than 26 chars, add new line
+                        title += "\n"
+                        line = 0
+                    title += entry
                 title = title[:-2]
                 plt.text(0.5, 1.05, title, fontsize=12, 
                          horizontalalignment='center', verticalalignment='center', transform=axs[row, column].transAxes)
-            if column == 0:
+            if column == 0 and "rows" in variables:
                 plt.ylabel(axes[1])
                 title = ""
                 for (key, value) in variables["rows"].items():
@@ -71,32 +136,9 @@ def plot_graph_line(df):
                 plt.text(-0.25, 0.5, title, fontsize=12, rotation=90,
                          horizontalalignment='center', verticalalignment='center', transform=axs[row, column].transAxes)
     fig.tight_layout()
-    plt.savefig("./figs/graph.png")
+    plt.savefig(storepath)
 
 if __name__ == '__main__':
-    # constants = {
-    #     "device": "cuda",
-    #     "delta": 0.00000005,
-    #     "clipping_threshold": 0.005, # how should I get C?
-    #     "weight_decay": 1e-5,
-    #     "setup": "ours",
-    #     "encoder_dimensions": "[128, 256, 256]",
-    #     "decoder_dimensions": "[256, 256, 349]",
-    #     "dropout": 0.1,
-    #     "dataset": "ogb_mag"
-    # }
-    # variables = ["lr", "noise_multiplier", "batch_size"]
-    # filepath = "./data/results_backup.csv"
-    # epsilon_bound = 8
-
-    # df = pd.read_csv(filepath)
-    # for (key, value) in constants.items():
-    #     df = df.loc[df[key] == value]
-
-    # graph_type = "best_heatmap"
-    # axes = ("degree_bound", "r_hop")
-    # values = ["test_acc", "train_acc"]
-
     df = pd.read_csv(filepath)
     for (key, value) in constants.items():
         df = df.loc[df[key] == value]
@@ -112,20 +154,10 @@ if __name__ == '__main__':
             df_i = pd.pivot_table(df, values=values[i], index=[axes[0]], columns=[axes[1]], aggfunc=np.mean)
             sns.heatmap(df_i, annot=True, fmt=".2%", vmax=0.3, vmin=0.22, ax=ax)
             ax.title(values[i] + " heatmap")
-        plt.savefig(storepath + "heatmap.png")
+        plt.savefig(storepath)
 
     elif graph_type == "best_heatmap":
-        
-        df = df.loc[df["epsilon"] <= epsilon_bound]
-        df = df.groupby([axes[0], axes[1], "step", *(variables["best"])], as_index=False).mean(numeric_only=True)
-        df = df.loc[df.groupby([axes[0], axes[1]])["epsilon"].idxmax()]
-        with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-            print(df)
-        df_i = pd.pivot_table(df, values=values[0], index=[axes[0]], columns=[axes[1]], aggfunc=np.max)
-        sns.heatmap(df_i, annot=True, fmt=".2%", vmax=0.3, vmin=0.22)
-        plt.title(values[0] + " heatmap")
-        plt.tight_layout()
-        plt.savefig("./figs/best_heatmap.png")
+        plot_heatmap(df)
 
     elif graph_type == "line":
         plot_graph_line(df)

preprocessor.py
@@ -2,32 +2,16 @@ import torch
 from torch_geometric.data import Data
 from configs.config import config
 
-# this method partitions based on nodes (so edges between splits are not used)
+# this method adds a train and test mask to dataset
 def train_test_split(dataset, test_ratio):
-    x, y, edge_index= dataset.x, dataset.y, dataset.edge_index
-    n = x.size(dim=0)
+    n = dataset.x.size(dim=0)
     shuffle_ordering = torch.randperm(n)
-    # edges need to be remapped based on shuffle ordering
-    # (i.e if nodes 3 and 4 are put in indices 5 and 2 respectively, 
-    #      edge 3 -> 4 would becomes 5 -> 2)
-    edge_mapping = torch.zeros(n, dtype=torch.long)
-    edge_mapping[shuffle_ordering] = torch.arange(n)
-    # shuffle x and y and apply to edge_index
-    x = x[shuffle_ordering]
-    y = y[shuffle_ordering]
-    edge_index = edge_mapping[edge_index]
     # mask based on train and test split
     mask = torch.zeros(n, dtype=torch.bool)
     train_slice = int((1-test_ratio)*n)
-    mask[:train_slice] = True
-    # apply split
-    x_train, x_test = x[mask], x[~mask]
-    y_train, y_test = y[mask], y[~mask]
-    # get edges for each split only if both nodes are in the respective split
-    edge_index_train = edge_index[:, torch.logical_and(*mask[edge_index])]
-    edge_index_test = edge_index[:, torch.logical_and(*~mask[edge_index])] - train_slice
-    return Data(x=x_train, y=y_train, edge_index=edge_index_train), \
-           Data(x=x_test, y=y_test, edge_index=edge_index_test)
+    mask[shuffle_ordering[:train_slice]] = True
+    dataset.train_mask = mask
+    dataset.test_mask = ~mask
 
 
 def apply_train_test_mask(dataset):
@@ -76,9 +60,10 @@ def get_adjacency_matrix(edge_index, num_nodes):
 
 
 def sample_edgelists(dataset, K):
-  dataset = dataset.to(config['device'])
+  dataset = dataset.clone().to(config['device'])
   x, y = dataset.x, dataset.y
   edge_index = dataset.edge_index
+  train_mask, test_mask = dataset.train_mask, dataset.test_mask
   A = get_adjacency_matrix(edge_index, x.size(dim=0))
   eps = 0.0000001
   out_degrees = torch.sparse.sum(A, dim=1).to_dense() + eps
@@ -94,13 +79,18 @@ def sample_edgelists(dataset, K):
   # filter x, y, and edge_index according to nodes with out-degree 
   # greater than K
   x, y = x[mask], y[mask]
+  train_mask, test_mask = train_mask[mask], test_mask[mask]
   edge_index = filter_edge_index(sampled_edge_index, mask)
-  return Data(x=x, y=y, edge_index=edge_index)
+  dataset.x, dataset.y = x, y
+  dataset.edge_index = edge_index
+  dataset.train_mask, dataset.test_mask = train_mask, test_mask
+  return dataset.cpu()
 
 
 def get_gradient_percentile(model, loss_fn, dataloader, percentile):
   # run one iteration without training
   batch = next(iter(dataloader))
+  batch = batch.to(config["device"])
   pred = model(batch)[:batch.batch_size]
   y = batch.y[:batch.batch_size]
   loss = loss_fn(pred, y)